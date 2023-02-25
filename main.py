ec580bfc5da722077dcaf4511447fb861ee46d84
MelnikovEI <MelnikovEI@ya.ru>
2023-02-25 20:53:02 +0300

    download book covers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+from urllib.parse import urljoin, urlsplit
 import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
@@ -23,23 +23,33 @@ def download_txt(url, filename, folder='books/'):
     Path(Path.cwd() / folder).mkdir(parents=True, exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
-    try:
-        check_for_redirect(response)
-    except requests.HTTPError:
-        return
     path = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
     with open(path, 'wb') as file_to_save:
         file_to_save.write(response.content)
     return path
 
 
-def main():
-    Path(Path.cwd() / 'books').mkdir(parents=True, exist_ok=True)
+def download_image(url, folder='images/'):
+    Path(Path.cwd() / folder).mkdir(parents=True, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
 
+    filename = urlsplit(url).path.split('/')[-1]
+    path = os.path.join(folder, sanitize_filename(filename))
+    with open(path, 'wb') as file_to_save:
+        file_to_save.write(response.content)
+    return path
+
+
+def main():
     for book_id in range(1, 11):
         url = f"https://tululu.org/b{book_id}/"
         response = requests.get(url)
         response.raise_for_status()
+        try:
+            check_for_redirect(response)
+        except requests.HTTPError:
+            continue
         soup = BeautifulSoup(response.text, 'lxml')
         title_tag = soup.find('h1')
         title_text = title_tag.text.split(sep='::')
@@ -47,6 +57,10 @@ def main():
         filename = f'{book_id}. {book_title}'
         txt_url = f"https://tululu.org/txt.php?id={book_id}"
         download_txt(txt_url, filename)
+
+        img_url = soup.find('div', class_='bookimage').find('img')['src']
+        full_img_url = urljoin("https://tululu.org/", img_url)
+        download_image(full_img_url)
 
 
 if __name__ == '__main__':
