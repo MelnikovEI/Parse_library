9d5ed4f57554be7ffbee37b061f990915cf014c1
MelnikovEI <MelnikovEI@ya.ru>
2023-02-25 09:51:38 +0300

    download books with titles

--- bs4_tutorial.py
+++ bs4_tutorial.py
@@ -1,14 +1,9 @@
-import os
+import os.path
 from pathlib import Path
+from pathvalidate import sanitize_filename
+from bs4 import BeautifulSoup
 
 import requests
-from bs4 import BeautifulSoup
-from pathvalidate import sanitize_filename
-
-
-def check_for_redirect(response):
-    if response.history:
-        raise requests.HTTPError
 
 
 def download_txt(url, filename, folder='books/'):
@@ -21,12 +16,9 @@ def download_txt(url, filename, folder='books/'):
         str: Путь до файла, куда сохранён текст.
     """
     Path(Path.cwd() / folder).mkdir(parents=True, exist_ok=True)
+
     response = requests.get(url)
     response.raise_for_status()
-    try:
-        check_for_redirect(response)
-    except requests.HTTPError:
-        return
     path = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
     with open(path, 'wb') as file_to_save:
         file_to_save.write(response.content)
@@ -34,19 +26,28 @@ def download_txt(url, filename, folder='books/'):
 
 
 def main():
-    Path(Path.cwd() / 'books').mkdir(parents=True, exist_ok=True)
+    url = f"https://tululu.org/b4/"
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find('h1')
+    title_text = title_tag.text.split(sep='::')
+    print(title_text[0].strip())
+    print(title_text[1].strip())
 
-    for book_id in range(1, 11):
-        url = f"https://tululu.org/b{book_id}/"
-        response = requests.get(url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'lxml')
-        title_tag = soup.find('h1')
-        title_text = title_tag.text.split(sep='::')
-        book_title = title_text[0].strip()
-        filename = f'{book_id}. {book_title}'
-        txt_url = f"https://tululu.org/txt.php?id={book_id}"
-        download_txt(txt_url, filename)
+    # print(soup.find('img', class_='attachment-post-image')['src'])
+
+    # Примеры использования
+    url = 'http://tululu.org/txt.php?id=1'
+
+    filepath = download_txt(url, 'Алиби')
+    print(filepath)  # Выведется books/Алиби.txt
+
+    filepath = download_txt(url, 'Али/би', folder='books/')
+    print(filepath)  # Выведется books/Алиби.txt
+
+    filepath = download_txt(url, 'Али\\би', folder='txt/')
+    print(filepath)  # Выведется txt/Алиби.txt
 
 
 if __name__ == '__main__':
