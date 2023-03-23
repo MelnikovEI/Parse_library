ff6764762d984643f73b650977b2569492c26fe3
MelnikovEI <MelnikovEI@ya.ru>
2023-03-23 21:08:16 +0300

    Add paginator

--- render_website.py
+++ render_website.py
@@ -1,10 +1,13 @@
 import json
+import math
 import os
 from pathlib import Path
 
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
+
+BOOKS_PER_PAGE = 20
 
 
 def rebuild():
@@ -17,12 +20,16 @@ def rebuild():
         books_json = books_file.read()
     books = json.loads(books_json)
     Path(Path.cwd() / 'pages').mkdir(parents=True, exist_ok=True)
-    for part_num, books_part in enumerate(chunked(books, 20), 1):
+    num_pages = math.ceil(len(books)/BOOKS_PER_PAGE)
+    for page_num, books_part in enumerate(chunked(books, BOOKS_PER_PAGE), 1):
         rendered_page = template.render(
             books=books_part,
+            current_page=page_num,
+            num_pages=num_pages,
         )
-        with open(os.path.join('pages', f'index{part_num}.html'), 'w', encoding="utf8") as file:
+        with open(os.path.join('pages', f'index{page_num}.html'), 'w', encoding="utf8") as file:
             file.write(rendered_page)
+    print(num_pages)
     print("Site rebuilt")
 
 
