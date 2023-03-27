3e7742323e211595db48545d490973f9d2304e93
MelnikovEI <MelnikovEI@ya.ru>
2023-03-27 19:23:04 +0300

    improve const name

--- render_website.py
+++ render_website.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
 
-BOOKS_PER_PAGE = 20
+BOOK_CARDS_PER_PAGE = 20
 
 
 def rebuild():
@@ -18,8 +18,8 @@ def rebuild():
         books_json = books_file.read()
     books = json.loads(books_json)
     Path(Path.cwd() / 'pages').mkdir(parents=True, exist_ok=True)
-    num_pages = math.ceil(len(books)/BOOKS_PER_PAGE)
-    for page_num, books_part in enumerate(chunked(books, BOOKS_PER_PAGE), 1):
+    num_pages = math.ceil(len(books) / BOOK_CARDS_PER_PAGE)
+    for page_num, books_part in enumerate(chunked(books, BOOK_CARDS_PER_PAGE), 1):
         rendered_page = template.render(
             books=books_part,
             current_page=page_num,
